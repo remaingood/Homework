be93ff503b28e4895c44b1434a5a6d96b65938fe
remaingood <john1kramer@mail.ru>
2017-12-01 16:23:12 +0300

    Initial Commit

---
+++ src/codingBat/string1/middleTwo/Solution.java
@@ -0,0 +1,16 @@
+package codingBat.string1.middleTwo;
+/*
+Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri". The string length will be at least 2.
+ */
+
+public class Solution {
+    public String middleTwo(String str) {
+        String res = "";
+        if(str.length() >=2 ){
+            res =  str.substring(str.length()/2 -1, str.length()/2 + 1);
+        }
+        return res;
+
+    }
+
+}
---
+++ src/codingBat/string1/theEnd/Solution.java
@@ -0,0 +1,15 @@
+package codingBat.string1.theEnd;
+/*
+Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.
+ */
+
+public class Solution {
+    public String theEnd(String str, boolean front) {
+        if(front){
+            return str.substring(0,1);
+        }
+
+        else return str.substring(str.length()-1, str.length());
+    }
+
+}
