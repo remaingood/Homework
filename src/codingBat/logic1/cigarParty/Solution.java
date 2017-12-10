1e3f1b1ade8f2407172a73cf24bf437b3685fc06
remaingood <john1kramer@mail.ru>
2017-12-10 15:41:53 +0300

    Initial Commit

---
+++ src/codingBat/logic1/cigarParty/Solution.java
@@ -0,0 +1,20 @@
+package codingBat.logic1.cigarParty;
+/*
+When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return true if the party with the given values is successful, or false otherwise.
+ */
+
+public class Solution {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if(!isWeekend){
+            if (cigars >= 40 && cigars <= 60){
+                return true;
+            }
+        }
+        else if(cigars >= 40){
+            return true;
+        }
+        return false;
+
+    }
+
+}
---
+++ src/codingBat/logic1/sortaSum/Solution.java
@@ -0,0 +1,16 @@
+package codingBat.logic1.sortaSum;
+/*
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+ */
+public class Solution {
+    public int sortaSum(int a, int b) {
+        int sum = a + b;
+        if(sum >= 10 && sum <=19){
+            return 20;
+        }
+
+        else return  sum;
+
+    }
+
+}
