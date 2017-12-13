6ea50ab7738e5a4ed72601ff53c36c0061a60236
remaingood <john1kramer@mail.ru>
2017-12-13 13:23:27 +0300

    Initial Commit

---
+++ src/codingBat/recursion1/bunnyEars/Solution.java
@@ -0,0 +1,14 @@
+package codingBat.recursion1.bunnyEars;
+/*
+
+We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+ */
+public class Solution {
+    public int bunnyEars(int bunnies) {
+        if (bunnies == 0){
+            return 0;
+        }
+        return 2 + bunnyEars(bunnies-1);
+
+    }
+}
